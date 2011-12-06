0c146d6e023e7279e8d750ad8c98ab1705cc9dd0
yrz <rudolphh.dev@gmail.com>
2011-12-05 19:19:45 -0800

    fixed routes

--- config/routes.php
+++ config/routes.php
@@ -13,12 +13,12 @@ $route['store/admin/categories(/:any)?']	= 'admin_categories$1';
 $route['store/admin/products(/:any)?']		= 'admin_products$1';
 
 // admin list products
-$route['store/admin/preview_product(/:any)?'] = 'store/product$1';
+$route['store/admin/preview/(:any)/(:any)'] = 'store/items/$1/$2';
 $route['store/admin/edit_product(/:any)?'] = 'admin_products/edit$1';
 $route['store/admin/delete_product(/:any)?'] = 'admin_products/delete$1';
 
 // admin list categories
-$route['store/admin/preview_category(/:any)?'] = 'store/category$1';
+$route['store/admin/preview(/:any)?'] = 'store/items$1';
 $route['store/admin/edit_category(/:any)?'] = 'admin_categories/edit$1';
 $route['store/admin/delete_category(/:any)?'] = 'admin_categories/delete$1';
 
